6df8468c1e598653d33a31944001c7d3dce579bf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-07-26 12:12:51 -0400

    set NODE_ENV=test for Cypress to instrument unit test

--- cypress/integration/first-spec.js
+++ cypress/integration/first-spec.js
@@ -1,18 +1,10 @@
 /// <reference types="cypress" />
 
-it('adds and completes todos', () => {
+it('adds todos', () => {
   cy.visit('/')
   cy.get('.new-todo')
     .type('write code{enter}')
     .type('write tests{enter}')
     .type('deploy{enter}')
   cy.get('.todo').should('have.length', 3)
-
-  cy.get('.todo')
-    .first()
-    .find('.toggle')
-    .check()
-  cy.get('.todo')
-    .first()
-    .should('have.class', 'completed')
 })
