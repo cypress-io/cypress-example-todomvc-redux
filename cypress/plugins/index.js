d66ca03466eb89283ee5b0a71763ae817fa41bb6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-06 16:14:30 -0400

    use latest code coverage plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,6 @@
 module.exports = (on, config) => {
-  on('task', require('@cypress/code-coverage/task'))
+  require('@cypress/code-coverage/task')(on, config)
+
   on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
 
   return config
