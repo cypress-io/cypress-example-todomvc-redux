1e3193f501733a5023d1c8570ef6e9666f4a6d5b
Emily Rohrbough <emilyrohrbough@yahoo.com>
2021-11-23 12:47:10 -0500

    update deps; match node versions; rm package-lock - CI uses yarn

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,12 @@
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config)
-
-  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+  if (config.testingType === 'component') {
+    console.log("HERE")
+    const injectDevServer = require('@cypress/react/plugins/react-scripts')
+    injectDevServer(on, config)
+  } else {
+    require('@cypress/code-coverage/task')(on, config)
+    on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+  }
 
   return config
 }
