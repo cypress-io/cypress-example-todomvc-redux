bb85ded79add156f39f4d195ab6fdd38986df94e
mjhenkes <mjhenkes@gmail.com>
2022-12-09 09:26:19 -0600

    disable old style component testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,21 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   env: {
-    'cypress-plugin-snapshots': {},
+    "cypress-plugin-snapshots": {},
   },
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config)
+      require("@cypress/code-coverage/task")(on, config);
 
-      on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
-    
-      return config
+      on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
+
+      return config;
     },
-    baseUrl: 'http://localhost:1234',
-    excludeSpecPattern: ['**/*.snap', '**/__snapshot__/*', '**/smoke.js'],
+    baseUrl: "http://localhost:1234",
+    excludeSpecPattern: ["**/*.snap", "**/__snapshot__/*", "**/smoke.js"],
   },
-})
+});
